5ecfb14f885dd1d616cbeeb6671a939ffb4d2689
Flávia Labanca <flavialabanca@gmail.com>
2025-02-11 17:02:13 -0300

    Adiciona script config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# config Oracle
+DB_CONFIG = {
+    "host": "host_name",   
+    "port": "1521",            # porta padrão Oracle
+    "service": "service_name",
+    "user": "user",
+    "password": "password"
+}
+
+# config PostgreSQL
+PG_CONFIG = {
+    "host": "host_name",  
+    "port": "5432",           # porta padrão PostgreSQL
+    "database": "SEU_BANCO",
+    "user": "user",
+    "password": "password"
+}
+
